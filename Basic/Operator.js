9a0935adfded97cb6b740871174667590a18ac2f
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-04-13 13:24:41 +0530

    Add files via upload

---
+++ Basic/Operator.js
@@ -0,0 +1,58 @@
+// Operator
+
+// Arithmetic
+console.log(2 + 2);
+console.log(2 - 5);
+console.log(2 * 3);
+console.log(6 / 2);
+console.log(2 % 5); // Reminder
+console.log(5 ** 2); //Multiplying 5 twise ( 5 * 5 = 25)
+
+// Assignment Operator
+
+let a = 10;
+let b = 90;
+x = a + b;
+x += y; //prefered way( same work as above)
+console.log(x);
+
+//Comparison Operator
+
+let x = 10;
+let y = 90;
+
+console.log(x > y); // return false
+console.log(x <= y); // return true
+console.log(x == y);
+console.log(x === y); // Strict check (Phele Type check hoga then the comparison will happen)
+
+// Converting DataTypes - String to number 
+
+let z = "10"; // Initially it is string 
+
+let c = Number(z); // converted into number 
+
+console.log(typeof c); // Checking its dataType
+
+let d = " 121ac"
+let e = Number(d);
+console.log(typeof e); // Return NAN
+//NAN : Not  a number(Type of it is number)
+
+// Number -> String
+let f = 10;
+let k = string(f);
+console.log(typeof f);
+
+
+
+
+
+
+
+
+
+
+
+
+
