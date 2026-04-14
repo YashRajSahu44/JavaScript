4ba75a21a7dabe56944fe6f8bb7dc40577baa6e4
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-04-14 13:08:35 +0530

    Add files via upload

---
+++ Basic/Condition.js
@@ -0,0 +1,24 @@
+let age = 15;
+
+if(age >= 18){
+    console.log("Eligible for vote");
+}
+else{
+    console.log("Not Eligible")
+}
+
+// Kid Young or Old
+/* Age < 18 -- Kid
+   Age > 18 -- Young
+   Age >= 60 -- Old  */
+
+   let age =18;
+   if(age < 18){
+    console.log("kid");
+   }
+   else if(age > 60){
+    console.log("Old");
+   }
+   else{
+    console.log("Young");
+   }
