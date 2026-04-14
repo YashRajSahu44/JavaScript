897fa0d02a8aac55adbd7724840e56f196a46c7c
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-04-14 13:08:57 +0530

    Add files via upload

---
+++ Basic/LogicalOperator.js
@@ -0,0 +1,41 @@
+// logical And&& , || 
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&false);
+// console.log(false&&true);
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||false);
+// console.log(false||true);
+
+// let a = "Rohit";
+// let b = "";
+// let c = a&&b;
+// console.log(c);
+
+// let a = 0;
+// let b = 20;
+
+// console.log(a&&b);
+// &&: If first value is false, it will return the first value itself
+// If first value is true, it will return second
+
+
+// || logical or
+
+let a = 0;
+let b = 20;
+
+console.log(a||b);
+// &&: If first value is true, it will return the first value itself
+// If first value is false, it will return second
+
+// !=
+// console.log(5!=5);
+
+
+// AND: & , | or
+
+console.log(2&5);
