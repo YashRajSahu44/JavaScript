8d8788a942455289631216e3ba19c304c93b500d
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-04-14 13:08:06 +0530

    Add files via upload

---
+++ Basic/Loop.js
@@ -0,0 +1,18 @@
+// For loop
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+// While loop
+let j = 0;
+while(j < 10){
+    console.log(j);
+    j++;
+}
+
+//Do-While loop
+let k = 0;
+do{
+    console.log(k);
+    k++
+}while(i < 10);
