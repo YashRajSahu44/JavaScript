098548fccadb036e15e628fbe8c0b7c2f700e361
YashRaj <yashsahu10th@gmail.com>
2026-05-24 22:24:16 +0530

    Covers all about number in js such as declearation

---
+++ Basic/number.js
@@ -0,0 +1,18 @@
+// let a = 10;
+// console.log(a);
+
+let b = 3434.4789
+console.log(b.toFixed(3)); // By toFixed it will only run till the third value after decimal 
+
+console.log(b.toPrecision(4)); // By toPrecision it will only print 3 values
+
+console.log(b.toString); //Will convert number into string
+
+// Another way of Declearing number
+
+let c = new Number(20);
+console.log(c); // Faltu way of declearing, not used much 
+// also it's an object 
+
+// Non primitive : Refrance bases par compare hote hai
+// Primitive : copy by value
